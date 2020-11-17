5c27424d2616344ba60ed75070535305101d472c
Marshall Bertschy <basemarsh@gmail.com>
2020-11-17 04:01:29 -0600

    added ajax call

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#searchButton").on("click", function (event) {
+    event.preventDefault()
+    var city = $(".form-control").val()
+    console.log(city)
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q={"+city+"}&appid={43c9d8f852db000dbda20adff5614d08}"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
+})
