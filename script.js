fda61c8b8610f430fb16a7d9bf5a139627790f0d
Marshall Bertschy <basemarsh@gmail.com>
2020-11-18 05:02:46 -0600

    added the current weather stats. still working on UV index box

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$( document ).ready(function() {
 $("#searchButton").on("click", function (event) {
     event.preventDefault()
     var city = $(".form-control").val()
@@ -7,6 +8,33 @@ $("#searchButton").on("click", function (event) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        $("#currentCity").text(response.name+" ("+moment().format('L')+")")
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $("#temp").text("Temperature: " + tempF.toFixed(1)+" °F")
+        $("#humidity").text("Humidity: "+response.main.humidity+"%")
+        $("#wind-speed").text("Wind Speed: "+response.wind.speed+" MPH")
         console.log(response)
+        var lat = response.coord.lat
+        var lon = response.coord.lon
+        console.log(lon)
+        console.log(lat)
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely&appid=43c9d8f852db000dbda20adff5614d08"
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function (response2) {
+            console.log(response2)
+            $("#uv-index").text("UV Index: "+response2.current.uvi)
+        })
     })
 })
+// var city = "san antonio"
+// var res = city.split(" ");
+// console.log(res)
+// for (var i = 0; i < res.length; i++) {
+//     console.log(res[i][0])
+// var firstLetter = res[i][0]
+// res[i][0] = firstLetter.toUpperCase()
+// console.log(res[i])
+// }
+});
