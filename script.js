2db69be9ebd98ed4b17fdaa88106a0f9c18fb7ce
Marshall Bertschy <basemarsh@gmail.com>
2020-11-17 18:04:20 -0600

    fixed my api call

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $("#searchButton").on("click", function (event) {
     event.preventDefault()
     var city = $(".form-control").val()
     console.log(city)
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q="+city+"&appid=43c9d8f852db000dbda20adff5614d08"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=43c9d8f852db000dbda20adff5614d08"
     $.ajax({
         url: queryURL,
         method: "GET"
